bf92f162a592f029db974644df10659d7fa3b4cd
Lenis Cervantes <leniserik@gmail.com>
2022-09-29 10:59:12 +0200

    const created

--- const/const.js
+++ const/const.js
@@ -1,5 +1,5 @@
 const difficulties = ['High', 'Medium-High', 'Medium', 'Medium-Low', 'Low']
-const skiStations = ['Baqueira-Beret', 'Boí Taüll', 'Formigal', 'Sierra Nevada', 'Cerler', 'Panticosa', 'Astún', 'Candanchú', ' La Pinilla', 'Valdesquí']
+const skiStations = ['Baqueira-Beret', 'Boí Taüll', 'Formigal', 'Sierra Nevada', 'Cerler', 'Panticosa', 'Astún', 'Candanchú', 'La Pinilla', 'Valdesquí']
 
 
 module.exports = {
